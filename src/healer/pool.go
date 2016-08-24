f9a9fe19090a18871564e9387cf8f9c6100a4ee5
András Gyácsok <andras.gyacsok@ericsson.com>
2016-08-24 17:28:53 +0200

    Started work with evacuation pool

--- src/healer/pool.go
+++ src/healer/pool.go
@@ -1,38 +1,42 @@
 package healer
-//
-//import "sync"
-//
-//type Worker interface {
-//	Task()
-//}
-//
-//type Pool struct {
-//	wg    sync.WaitGroup
-//	tasks chan Worker
-//}
-//
-//func NewPool(numProcesses int) *Pool {
-//	p := &Pool{
-//		wg: sync.WaitGroup{numProcesses},
-//		tasks: make(chan Worker),
-//	}
-//
-//	for i:=0; i < numProcesses; i++ {
-//		go func(){
-//			for w := range p.tasks {
-//				w.Task()
-//			}
-//			p.wg.Done()
-//		}()
-//	}
-//	return &p
-//}
-//
-//func (p *Pool) Run(task Worker) {
-//	p.tasks <- task
-//}
-//
-//func (p *Pool) Shutdown() {
-//	close(p.tasks)
-//	p.wg.Wait()
-//}
+
+import "sync"
+
+type Worker interface {
+	Task()
+}
+
+type Pool struct {
+	wg    sync.WaitGroup
+	tasks chan Worker
+}
+
+// TODO: Add logic of two chanals
+// TODO: receive VMs for evacuation
+// TODO: send info about evacuation
+func NewPool(numProcesses int) *Pool {
+	p := &Pool{
+		tasks: make(chan Worker),
+	}
+
+	p.wg.Add(numProcesses)
+
+	for i:=0; i < numProcesses; i++ {
+		go func(){
+			for w := range p.tasks {
+				w.Task()
+			}
+			p.wg.Done()
+		}()
+	}
+	return p
+}
+
+func (p *Pool) Run(task Worker) {
+	p.tasks <- task
+}
+
+func (p *Pool) Shutdown() {
+	close(p.tasks)
+	p.wg.Wait()
+}
